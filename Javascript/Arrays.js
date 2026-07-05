cc0c85b7c76dd98ea2f9d5553d772be4c1163798
Abdullah Hussain Mallah <abdullahmallah468@gmail.com>
2026-07-05 09:25:38 +0500

    Arrays

--- Javascript/Arrays.js
+++ Javascript/Arrays.js
@@ -1,6 +1,11 @@
 let marks = [93,79,73,78,87];
 let len = marks.length;
-console.log("Marks: ");
-for(let i=0;i<len;i++){
-    console.log(marks[i]+" ");
+marks[0] = 98;
+var sum = 0;
+for(let mark of marks){
+    sum = sum + mark;
 }
+console.log("Sum of the marks: ",sum);
+let avg = (sum/len);
+
+console.log("Average of the marks: ",avg);
