e12f5ae02efa5cb55efd33ff1cf9820e4f19acf9
Abdullah Hussain Mallah <abdullahmallah468@gmail.com>
2026-07-05 09:46:52 +0500

    Array methods

---
+++ Javascript/arrayMethods.js
@@ -0,0 +1,9 @@
+let pushExercises = ["Standard Pushups","Diamond Pushups","Incline Pushups","Decline Pushups"];
+
+pushExercises.push("Archer Pushups","Dips");
+
+console.log(pushExercises);// will add two new 
+
+pushExercises.pop();// will remove last one "Dips"
+
+console.log(pushExercises);
